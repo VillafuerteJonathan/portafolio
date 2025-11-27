4bb78a144169399b3aaadd0a9b0c25c275ac6238
VillafuerteJonathan <86273066+VillafuerteJonathan@users.noreply.github.com>
2025-11-27 01:33:14 -0500

    repositorios listos y llenos

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -3,42 +3,57 @@
 export const PROJECTS = [
     {
         id: 1,
-        title: "Catalogo de calzado Strong ",
-        desc: "Plataforma basado en catalogo de calzado industria;.",
+        title: "Catálogo de Calzado Strong",
+        desc: "Plataforma web basada en un catálogo digital de calzado industrial.",
         img: "/assets/images/strong.png",
-        tech: ["React + vite", "Node.js", "MySQL", "Typescript", "CSS"],
+        tech: ["React + Vite", "Node.js", "MySQL", "TypeScript", "CSS"],
         code: "https://github.com/VillafuerteJonathan/Strong",
         video: "/assets/videos/strong.mp4",
-
     },
     {
         id: 2,
-        title: "Menu Restaurante Tradicion y tierra ",
-        desc: "Aplicacion resposiva para mostrar el menu de un restaurante.",
+        title: "Menú Restaurante Tradición y Tierra",
+        desc: "Aplicación responsive para mostrar el menú completo de un restaurante.",
         img: "/assets/images/restaurante.png",
-        tech: ["React", "Node", "JavaScript","Mysql","CSS"],
+        tech: ["React", "Node.js", "JavaScript", "MySQL", "CSS"],
         code: "https://github.com/VillafuerteJonathan/RestaruranteTradicionyTierra",
         video: "/assets/videos/restaurante.mp4",
-       
+    },
+     {
+        id: 3,
+        title: "Portafolio Personal",
+        desc: "Portafolio desarrollado con un nuevo lenguajepor el desarrollador para mostrar habilidades, experiencia y proyectos.",
+        img: "/assets/images/Portafolio.png",
+        tech: ["Svelte", "Vite", "Tailwind", "TypeScript"],
+        code: "https://github.com/OscarJRM/financial-simulator-app",
+        video: "/assets/videos/Portafolio.mp4",
     },
     {
-        id: 3,
-        title: "Portfolio CMS",
-        desc: "Sistema de gestión de contenidos para portafolios con editor visual y generación automática de páginas.",
-        img: "/assets/images/plantaenferma.png",
-        tech: ["Andoid Studio", "Python", "IA","TensorFlow / Keras" ,"Jupyter Notebook"],
-        code: "https://github.com/turepo/portfolio-cms",
-        video: "/assets/videos/plantaenferma.mp4",
-        
+        id: 4,
+        title: "Bóveda de Archivos Segura 🔐",
+        desc: "Sistema para cargar, cifrar, gestionar permisos y almacenar archivos de manera segura.",
+        img: "/assets/images/seguridad.png",
+        tech: ["Node.js", "React", "CORS", "JWT / OTP", "Crypto"],
+        code: "https://github.com/VillafuerteJonathan/seguridad1",
+        video: "/assets/videos/seguridad.mp4",
     },
     {
-        id: 3,
-        title: "Portfolio CMS",
-        desc: "Sistema de gestión de contenidos para portafolios con editor visual y generación automática de páginas.",
+        id: 5,
+        title: "Reconocimiento de Enfermedades en hojas de Plantas",
+        desc: "Aplicación móvil que identifica enfermedades en hojas de tomate en tiempo real utilizando IA.",
         img: "/assets/images/plantaenferma.png",
-        tech: ["Andoid Studio", "Python", "IA","TensorFlow / Keras" ,"Jupyter Notebook"],
-        code: "https://github.com/turepo/portfolio-cms",
+        tech: ["Android Studio", "Python", "IA", "TensorFlow / Keras", "Jupyter Notebook"],
+        code: "https://github.com/LizardCs/ModelosReconocimientoImagenes",
         video: "/assets/videos/plantaenferma.mp4",
-        
+    },
+    {
+        id: 6,
+        title: "Financial Simulator App",
+        desc: "Simulador financiero que genera tablas de amortización utilizando distintos sistemas de cálculo.",
+        img: "/assets/images/SimuladorCredito.png",
+        tech: ["Next.js", "NestJS", "Tailwind", "Docker", "TypeScript"],
+        code: "https://github.com/OscarJRM/financial-simulator-app",
+        video: "/assets/videos/SimuladorCredito.mp4",
     }
-];  
+   
+];
