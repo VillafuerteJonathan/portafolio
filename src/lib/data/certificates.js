08b9c0fbc87c9a72dc2b13986f7aaa3a159b4fcd
VillafuerteJonathan <86273066+VillafuerteJonathan@users.noreply.github.com>
2026-05-13 16:36:11 -0500

    actualizacion de certificados reacomodacion

--- src/lib/data/certificates.js
+++ src/lib/data/certificates.js
@@ -1,37 +1,37 @@
 export const CERTIFICATES = [
     {
         title: "Aplicaciones del entorno POWER BI",
-        image: "/assets/certificates/EntornoPowerBI.jpg", // Reemplaza con la ruta correcta
+        image: "../../EntornoPowerBI.jpg", // Reemplaza con la ruta correcta
         institution: "SECAP",
         date: "2026"
     },
     {
         title: "Curso Práctico: Branding, Community Manager y Diseño Publicitario",
-        image: "/assets/certificates/CursoPractico.jpg", // Reemplaza con la ruta correcta
+        image: "../../certificates/CursoPractico.jpg", // Reemplaza con la ruta correcta
         institution: "Universidad Técnica de Ambato (UTA)",
         date: "2025"
     },
     {
         title: "Hacker Ético",
-        image: "/assets/certificates/HackerEtico.jpg", // Reemplaza con la ruta correcta
+        image: "../../certificates/HackerEtico.jpg", // Reemplaza con la ruta correcta
         institution: "Cisco Networking Academy",
         date: "2025"
     },
     {
         title: "Fundamentos JavaScript 1",
-        image: "/assets/certificates/JavaScript.jpg", // Reemplaza con la ruta correcta
+        image: "../../certificates/JavaScript.jpg", // Reemplaza con la ruta correcta
         institution: "Cisco Networking Academy",
         date: "2025"
     },
     {
         title: "Activa tu modo lider educativo",
-        image: "/assets/certificates/EncuentroPedagogico.jpg", // Reemplaza con la ruta correcta
+        image: "../../certificates/EncuentroPedagogico.jpg", // Reemplaza con la ruta correcta
         institution: "Grupo R.A.",
         date: "2026"
     },
     {
         title: "Título de Conductor Profesional - Licencia Tipo C",
-        image: "/assets/certificates/ChoferProfesional.jpg", // Reemplaza con la ruta correcta
+        image: "../../certificates/ChoferProfesional.jpg", // Reemplaza con la ruta correcta
         institution: "Agencia Nacional de Tránsito (ANT)",
         date: "2025"
     },
@@ -39,7 +39,7 @@ export const CERTIFICATES = [
 
     {
         title: "Prácticas Preprofesionales: Desarrollo de Sistema para Cementerios Municipales",
-        image: "/assets/certificates/PracticasPillaro.jpg", // Reemplaza con la ruta correcta
+        image: "../../certificates/PracticasPillaro.jpg", // Reemplaza con la ruta correcta
         institution: "GAD Municipal del Cantón Santiago de Píllaro",
         date: "2025"
     },
