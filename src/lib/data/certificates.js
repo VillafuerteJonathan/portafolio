7bef809c269f05bab4274c3a01674a13a9291dac
VillafuerteJonathan <86273066+VillafuerteJonathan@users.noreply.github.com>
2026-05-13 16:16:53 -0500

    actualizacion de certificados

--- src/lib/data/certificates.js
+++ src/lib/data/certificates.js
@@ -1,5 +1,11 @@
 export const CERTIFICATES = [
-      {
+    {
+        title: "Aplicaciones del entorno POWER BI",
+        image: "/assets/certificates/EntornoPowerBI.jpg", // Reemplaza con la ruta correcta
+        institution: "SECAP",
+        date: "2026"
+    },
+    {
         title: "Curso Práctico: Branding, Community Manager y Diseño Publicitario",
         image: "/assets/certificates/CursoPractico.jpg", // Reemplaza con la ruta correcta
         institution: "Universidad Técnica de Ambato (UTA)",
@@ -11,11 +17,17 @@ export const CERTIFICATES = [
         institution: "Cisco Networking Academy",
         date: "2025"
     },
-     {
+    {
         title: "Fundamentos JavaScript 1",
         image: "/assets/certificates/JavaScript.jpg", // Reemplaza con la ruta correcta
         institution: "Cisco Networking Academy",
         date: "2025"
+    },
+    {
+        title: "Activa tu modo lider educativo",
+        image: "/assets/certificates/EncuentroPedagogico.jpg", // Reemplaza con la ruta correcta
+        institution: "Grupo R.A.",
+        date: "2026"
     },
     {
         title: "Título de Conductor Profesional - Licencia Tipo C",
@@ -23,14 +35,19 @@ export const CERTIFICATES = [
         institution: "Agencia Nacional de Tránsito (ANT)",
         date: "2025"
     },
-     
+
+
     {
         title: "Prácticas Preprofesionales: Desarrollo de Sistema para Cementerios Municipales",
         image: "/assets/certificates/PracticasPillaro.jpg", // Reemplaza con la ruta correcta
         institution: "GAD Municipal del Cantón Santiago de Píllaro",
         date: "2025"
-    }
-  
-   
-    
+    },
+
+
+
+
+
+
+
 ];
